2cf25e0c93e1486e7d77e204b2b5cbb882705caa
kitesoul99 <47879981+kitesoul99@users.noreply.github.com>
2020-04-10 15:41:53 +0800

    Update Superspeed.sh

--- Superspeed.sh
+++ Superspeed.sh
@@ -136,6 +136,12 @@ runtest() {
 		start=$(date +%s) 
 
 		 speed_test '3633' '上海' '电信'
+		 speed_test '26180' '山东济南５Ｇ' '联通'
+		 speed_test '26678' '广东广州５Ｇ' '联通'
+		 speed_test '27151' '山东临沂５Ｇ' '移动'
+		 speed_test '25881' '山东济南５Ｇ' '移动'
+		 speed_test '27100' '山东青岛５Ｇ' '移动'
+		 speed_test '26501' '山西太原５Ｇ' '移动'
 		 speed_test '24012' '内蒙古呼和浩特' '电信'
 		 speed_test '27377' '北京５Ｇ' '电信'
 		 speed_test '29026' '四川成都' '电信'
